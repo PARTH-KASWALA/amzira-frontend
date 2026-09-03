57945e16914a06bf836243d8fb3c3ad154fbd4fb
PARTH-KASWALA <parthkaswala2004@gmail.com>
2026-09-03 11:40:47 +0530

    Add optional frontend Sentry instrumentation

--- app/error.tsx
+++ app/error.tsx
@@ -3,10 +3,12 @@
 import Link from "next/link";
 import { AlertCircle, RefreshCw } from "lucide-react";
 import { useEffect } from "react";
+import * as Sentry from "@sentry/nextjs";
 
 export default function GlobalError({ error, reset }: { error: Error & { digest?: string }; reset: () => void }) {
   useEffect(() => {
     console.error(error);
+    Sentry.captureException(error);
   }, [error]);
 
   return (
